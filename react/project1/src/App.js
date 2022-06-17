bf3e266f0994912f49b1a776ef71655037e95945
Nahyun-Kweon <23xianx@gmail.com>
2022-06-17 18:55:05 +0900

    make dashboard
    따릉이 대시보드

---
+++ react/project1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hello from "./Hello";
+
+function App() {
+  
+  return (
+    /* jshint ignore:start */
+    <>
+      <Hello />
+    </>
+    /* jshint ignore:end */
+  );
+}
+export default App;
